fbfa002f046f18f52a1d962004db3b36d42977ae
Oskars Braucs <oskars.braucs@gmail.com>
2023-12-12 11:45:04 +0200

    Add Stripe billing portal

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :posts
   post "checkout/create", to: "checkout#create", as: "checkout_create"
   get "checkout/create", to: "checkout#create"
+  post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
+  get "billing_portal/create", to: "billing_portal#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
